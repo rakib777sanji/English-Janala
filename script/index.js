c3722fb365fef8a1a3b7de7875350c52ac707211
rakib777sanji <litonpramanik3525@gmail.com>
2025-10-23 17:53:42 -1200

    completing api response to ui for lesson section

--- script/index.js
+++ script/index.js
@@ -4,6 +4,30 @@ const loadlessons = () => {
     .then((json) => displayLesson(json.data));
 };
 
+const loadLevelWord = (id) => {
+  const url = `https://openapi.programming-hero.com/api/level/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayLevelWord(data.data));
+};
+
+const displayLevelWord = (words) => {
+  const wordContainer = document.getElementById("word-container");
+  //   wordContainer.innerHTML = "";
+
+  words.forEach((word) => {
+    console.log(word);
+    const card = document.createElement("div");
+    card.innerHTML = `
+    
+    <p> Cat </p>
+
+    `;
+
+    wordContainer.append(card);
+  });
+};
+
 const displayLesson = (lessons) => {
   //  1 get the container and empty
   const levelContainer = document.getElementById("level-container");
@@ -15,7 +39,7 @@ const displayLesson = (lessons) => {
     // 3 create element
     const btnDiv = document.createElement("div");
     btnDiv.innerHTML = `
-     <button class="btn btn-outline btn-primary">
+     <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary">
      <i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no} </button>
 
     `;
