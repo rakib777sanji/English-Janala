168220934a2b3c43337f90d2ec254298765e3d24
rakib777sanji <litonpramanik3525@gmail.com>
2025-10-23 16:03:37 -1200

    completed lesson styles from api

---
+++ script/index.js
@@ -0,0 +1,28 @@
+const loadlessons = () => {
+  fetch("https://openapi.programming-hero.com/api/levels/all")
+    .then((res) => res.json())
+    .then((json) => displayLesson(json.data));
+};
+
+const displayLesson = (lessons) => {
+  //  1 get the container and empty
+  const levelContainer = document.getElementById("level-container");
+  levelContainer.innerHTML = "";
+
+  //  2 get into every lesson
+  for (let lesson of lessons) {
+    console.log(lesson);
+    // 3 create element
+    const btnDiv = document.createElement("div");
+    btnDiv.innerHTML = `
+     <button class="btn btn-outline btn-primary">
+     <i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no} </button>
+
+    `;
+
+    // 4 append the container
+    levelContainer.append(btnDiv);
+  }
+};
+
+loadlessons();
